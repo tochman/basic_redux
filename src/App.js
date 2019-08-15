a1df83fba531b79e8d7bfcd0a012def48d64e4fb
Thomas <thomas@craftacademy.se>
2019-08-15 15:08:34 +0200

    adds hello world from application state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import { Container, Header } from 'semantic-ui-react'
-
-
+import { connect } from 'react-redux'
 
 class App extends Component {
-  state = {
-    greeting: 'Hello World from Component State'
-  }
-
   render() {
     return (
       <>
         <Container>
-          <Header as="h1">{this.state.greeting}</Header>
+          <Header as="h1">{this.props.greeting}</Header>
         </Container>
       </>
     )
   }
-
-
+}
+const mapStateToProps = (state) => {
+  return {
+    greeting: state.greeting
+  }
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
