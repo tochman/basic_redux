cceee88f46de4b85d5a33ba73378cb1164e0de84
Thomas <thomas@craftacademy.se>
2019-08-16 17:01:12 +0200

    reverts App component back to function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from 'semantic-ui-react'
 import Greeting from './Greeting'
 import GreetingInput from './GreetingInput'
 
 
-class App extends Component {
-  render() {
-
-    return (
-      <>
-        <Container>
-          <Greeting />
-          <GreetingInput />
-        </Container>
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <>
+      <Container>
+        <Greeting />
+        <GreetingInput />
+      </Container>
+    </>
+  )
 }
 
 export default App;
